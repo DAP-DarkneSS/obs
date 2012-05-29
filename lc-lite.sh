b380d6ed614037ae265613bb3f36be73939e03ce
Me via pythonanywhere.com <dap.darkness@gmail.com>
2012-05-29 20:33:23 +0000

    Formatting parsing at https://www.pythonanywhere.com was fixed.

--- lc-lite.sh
+++ lc-lite.sh
@@ -12,18 +12,18 @@ git pull
 VGIT=`git describe`
 echo -e '\e[0;33m\n'$VGIT'\e[0m'
 
-echo -e '\e[0;4m\nChecking of OBS version:\e[0m'
+echo -e "\n"'\e[0;4mChecking of OBS version:\e[0m'
 
 VOBS=`curl -s $SPEC | grep 'define LEECHCRAFT' | awk '{print $3}'`
 echo -e '\e[0;33m\n'$VOBS'\e[0m'
 
-echo -e '\e[0;4m\nChecking of OBS status:\n\e[0m'
+echo -e "\n"'\e[0;4mChecking of OBS status:\e[0m'"\n"
 curl -s curl -s $MONI | tr '"' ' ' | grep $NOBS | awk '{print $5 "\t" $7}'
 
 if [ "$VGIT" == "$VOBS" ]
 
 then
-  echo -e '\e[0;4m\nNo changes.\e[0m'
+  echo -e "\n"'\e[0;4mNo changes.\e[0m'
 
 else
   echo -e '\e[0;35m\nThe spec could be edited!\e[0m'
