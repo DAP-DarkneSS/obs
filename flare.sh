d95b46ed1a507633cde1c3628bdfac75d89e5b16
Dmitriy <dap@open.by>
2012-07-20 00:29:27 +0300

    Commiting files only from updated project.

--- flare.sh
+++ flare.sh
@@ -42,7 +42,7 @@ else
 
   echo -e '\e[0;4m\nGit will be updated...\e[0m'
   cd ../..
-  git commit -a -m 'Flare '$VGIT'.'
+  git commit $DOBS -m 'Flare '$VGIT'.'
   kdialog --title "SSH" --passivepopup "Enter passphrase for key!"
   git push -u origin master
 
