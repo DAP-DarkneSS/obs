256f26142b83ce4ff676e72edbcfacaf330d2bf4
Dmitriy <dap@open.by>
2012-12-02 21:07:17 +0300

    rexloader.sh was updated because of OBS update.

--- rexloader.sh
+++ rexloader.sh
@@ -6,9 +6,9 @@ FPRJ=$OPRJ':fedora'
 MPRJ=$OPRJ':mandriva'
 DPRJ=$OPRJ':deb'
 DSVN=~/Documents/obs/trash/$NOBS
-# DOBS=~/Documents/obs/home:DarkSS/$NOBS
-# DMBS=~/Documents/obs/home:DarkSS:mandriva/$NOBS
-# DFBS=~/Documents/obs/home:DarkSS:fedora/$NOBS
+DOBS=~/Documents/obs/home:DarkSS/$NOBS
+DMBS=~/Documents/obs/home:DarkSS:mandriva/$NOBS
+DFBS=~/Documents/obs/home:DarkSS:fedora/$NOBS
 
 echo -e '\e[0;4mChecking of google code version:\e[0m'
 cd $DSVN
@@ -18,9 +18,7 @@ VSVN=`svnversion | grep -o '[0-9]*'`
 echo -e '\nrevision #\e[0;33m'$VSVN'\e[0m'
 
 echo -e '\e[0;4m\nChecking of OBS version:\e[0m'
-# cd $DOBS
-# osc up
-# VOBS=`grep Revision $NOBS.spec -m 1 | grep -o '[0-9]*'`
+
 VOBS=`osc ls -b $OPRJ $NOBS | grep -m 1 svn | awk 'BEGIN {FS="[.,-]"} {print $5}'`
 echo -e '\nrevision #\e[0;33m'$VOBS'\e[0m'
 
@@ -31,47 +29,15 @@ then
 
 else
 
-#   echo -e '\e[0;4m\nShould the spec be edited?\e[0m'
-#   read
-# 
-#   CTIME=`date +%g'.'%m'.'%d'-'%H.%M.%S`
-# 
-#   function spec {
-#   cp ./$NOBS.spec ./$NOBS.spec.$VOBS.$CTIME
-#   YOBS='\n'`LANG=en_GB.UTF-8 date +'* '%a' '%b' '%d' '%Y' DA <dap.darkness@gmail.com> - '%Y%m%d'-1'`'\n- Revision #'$VSVN'.\n'
-#   sed "s/changelog$/&$YOBS/g" ./$NOBS.spec.$VOBS.$CTIME > ./$NOBS.spec
-#   }
-# 
-#   spec
-# 
-#   cd $DMBS
-#   osc up
-#   spec
-# 
-#   cd $DFBS
-#   osc up
-#   spec
-
   echo -e '\e[0;4m\nShould services be run?\e[0m'
   read
 
   for NPRJ in $OPRJ $FPRJ $MPRJ $DPRJ
 
   do
-    osc service rr $NPRJ $NOBS
+    cd ~/Documents/obs/$NPRJ/$NOBS
+    osc service rr
+    osc up
   done
-#   osc ci -m 'Revision #'$VSVN'.'
-# 
-#   cd $DMBS
-#   osc ci -m 'Revision #'$VSVN'.'
-# 
-#   cd $DOBS
-#   osc ci -m 'Revision #'$VSVN'.'
-# 
-#   echo -e '\e[0;4m\nGit will be updated...\e[0m'
-#   cd ../..
-#   git commit -a -m 'Rexloader rev'$VSVN'.'
-#   kdialog --title "SSH" --passivepopup "Enter passphrase for key!"
-#   git push -u origin master
 
 fi
